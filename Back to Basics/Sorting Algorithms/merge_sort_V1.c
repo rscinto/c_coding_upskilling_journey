9efb6bda2eb84410a876c732694012b03aa19050
Rocco <54637168+rscinto@users.noreply.github.com>
2026-05-16 15:37:34 -0700

    finished first version of merge sort

---
+++ Back to Basics/Sorting Algorithms/merge_sort_V1.c
@@ -0,0 +1,71 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void merge(int arr[], const size_t left, const size_t middle, const size_t right) {
+
+    const size_t num_elements = (right - left) + 1;
+    int temp_array[num_elements];
+    size_t temp_index = 0;
+    size_t left_index = left;
+    size_t right_index = middle+1;
+    bool right_complete = false;
+
+    do {
+        if ( right_complete || ((left_index <= middle) && (arr[left_index] <= arr[right_index])) ) {
+            temp_array[temp_index] = arr[left_index];
+            temp_index++;
+            left_index++;
+        }
+        else {
+            temp_array[temp_index] = arr[right_index];
+            temp_index++;
+            right_index++;
+            if (right_index > right) {
+                right_complete = true;
+            }
+        }
+    }while (temp_index != num_elements);
+
+    //copy everything back
+    temp_index=0;
+    for (size_t i = left; i <= right; i++) {
+        arr[i] = temp_array[temp_index++];
+    }
+}
+
+void merge_sort(int arr[], const size_t left, const size_t right)
+{
+    if (left >= right)
+    {
+        return;
+    }
+
+    const size_t middle = (left + right) / 2;
+
+    merge_sort(arr, left, middle);
+    merge_sort(arr, middle + 1, right);
+
+    merge(arr, left, middle, right);
+}
+
+int main(void) {
+    const size_t INPUT_SIZE = 10;
+    int input[] = {4, 5, 1, 2, 7, 3, 9, 6, 8, 0};
+
+    printf("Input:");
+    for (size_t i = 0; i < INPUT_SIZE; i++ ) {
+        printf(" %d ", input[i]);
+    }
+
+    merge_sort(input, 0, INPUT_SIZE - 1);
+
+    printf("Output:");
+    for (size_t i = 0; i < INPUT_SIZE; i++ ) {
+        printf(" %d ", input[i]);
+    }
+
+    return 0;
+}
