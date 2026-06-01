8d4bb3a6934a83ae5060a406fd53a1dd3b6995a9
Rocco <54637168+rscinto@users.noreply.github.com>
2026-06-01 16:46:14 -0700

    updated SCD4X driver with check sum functions. Modded commands with defines. Modded defines to avoid naming conflicts.

--- Drivers/SCD4X_temp_hum_c02/SCD4X.c
+++ Drivers/SCD4X_temp_hum_c02/SCD4X.c
@@ -8,6 +8,40 @@ uint8_t read_measurement_CMD[2] = {0xec, 0x05};
 uint8_t stop_periodic_measurement_CMD[2] = {0x3f, 0x86};
 
 
+//Provided function written by SENSIRION
+static uint8_t SCD4X_generate_crc(uint8_t msb, uint8_t lsb) {
+	uint8_t data[2] = {msb, lsb};
+	uint16_t current_byte;
+	uint8_t crc = CRC8_INIT;
+	uint8_t crc_bit;
+	/* calculates 8-Bit checksum with given polynomial */
+	for (current_byte = 0; current_byte < 2; ++current_byte) {
+		crc ^= (data[current_byte]);
+		for (crc_bit = 8; crc_bit > 0; --crc_bit) {
+			if (crc & 0x80)
+				crc = (crc << 1) ^ CRC8_POLYNOMIAL;
+			else
+				crc = (crc << 1);
+		}
+	}
+	return crc;
+}
+
+
+
+
+
+
+static bool SCD4X_crc_ok(uint8_t msb, uint8_t lsb, uint8_t received_crc)
+{
+	return (received_crc) == SCD4X_generate_crc(msb,lsb);
+}
+
+
+
+
+
+
 static HAL_StatusTypeDef SCD4X_send_command(uint16_t command)
 {
     uint8_t cmd[2];
@@ -35,7 +69,7 @@ void SCD4X_init(I2C_HandleTypeDef *hi2c) {
 }
 
 HAL_StatusTypeDef  SCD4X_start_periodic_measurement(){
-	return SCD4X_send_command(0x21B1);
+	return SCD4X_send_command(SCD4X_CMD_START_PERIODIC_MEASUREMENT);
 }
 
 
@@ -45,11 +79,17 @@ HAL_StatusTypeDef  SCD4X_start_periodic_measurement(){
 
 HAL_StatusTypeDef SCD4X_read_measurement(SCD4X_Measurement_t *out)
 {
+
+    if (out == NULL)
+    {
+        return HAL_ERROR;
+    }
+
     uint8_t rx[9];
 
     HAL_StatusTypeDef status;
 
-    status = SCD4X_send_command(0xEC05);
+    status = SCD4X_send_command(SCD4X_CMD_READ_MEASUREMENT);
     if (status != HAL_OK)
     {
         return status;
@@ -69,6 +109,24 @@ HAL_StatusTypeDef SCD4X_read_measurement(SCD4X_Measurement_t *out)
     {
         return status;
     }
+
+
+    if(!SCD4X_crc_ok( rx[0], rx[1],  rx[2]))
+    {
+    	return HAL_ERROR;
+    }
+
+    if(!SCD4X_crc_ok( rx[3], rx[4],  rx[5]))
+    {
+    	return HAL_ERROR;
+    }
+
+    if(!SCD4X_crc_ok( rx[6], rx[7],  rx[8]))
+    {
+    	return HAL_ERROR;
+    }
+
+
     //promote uint8 to uint16. fill with MSB. shift left 8 bits. fill lower 8 bits with LSB by using OR operatation
     uint16_t raw_co2  = ((uint16_t)rx[0] << 8) | rx[1];
     uint16_t raw_temp = ((uint16_t)rx[3] << 8) | rx[4];
@@ -86,7 +144,7 @@ HAL_StatusTypeDef SCD4X_read_measurement(SCD4X_Measurement_t *out)
 
 
 HAL_StatusTypeDef  SCD4X_stop_periodic_measurement(){
-	return SCD4X_send_command(0x3F86);
+	return SCD4X_send_command(SCD4X_CMD_STOP_PERIODIC_MEASUREMENT);
 }
 
 
@@ -94,20 +152,8 @@ HAL_StatusTypeDef  SCD4X_stop_periodic_measurement(){
 
 
 
-//Provided function written by SENSIRION
-uint8_t SCD4X_generate_crc(const uint8_t *data, uint16_t count) {
-	uint16_t current_byte;
-	uint8_t crc = CRC8_INIT;
-	uint8_t crc_bit;
-	/* calculates 8-Bit checksum with given polynomial */
-	for (current_byte = 0; current_byte < count; ++current_byte) {
-		crc ^= (data[current_byte]);
-		for (crc_bit = 8; crc_bit > 0; --crc_bit) {
-			if (crc & 0x80)
-				crc = (crc << 1) ^ CRC8_POLYNOMIAL;
-			else
-				crc = (crc << 1);
-		}
-	}
-	return crc;
-}
+
+
+
+
+
--- Drivers/SCD4X_temp_hum_c02/SCD4X.h
+++ Drivers/SCD4X_temp_hum_c02/SCD4X.h
@@ -18,6 +18,27 @@
 #define CRC8_POLYNOMIAL 0x31
 #define CRC8_INIT 0xFF
 
+#define SCD4X_CMD_START_PERIODIC_MEASUREMENT              0x21B1
+#define SCD4X_CMD_READ_MEASUREMENT                        0xEC05
+#define SCD4X_CMD_STOP_PERIODIC_MEASUREMENT               0x3F86
+#define SCD4X_CMD_SET_TEMPERATURE_OFFSET                  0x241D
+#define SCD4X_CMD_GET_TEMPERATURE_OFFSET                  0x2318
+#define SCD4X_CMD_SET_AMBIENT_PRESSURE                    0xE000
+#define SCD4X_CMD_PERFORM_FORCED_RECALIBRATION            0x362F
+#define SCD4X_CMD_SET_AUTOMATIC_SELF_CALIBRATION_ENABLED  0x2416
+#define SCD4X_CMD_GET_AUTOMATIC_SELF_CALIBRATION_ENABLED  0x2313
+#define SCD4X_CMD_START_LOW_POWER_PERIODIC_MEASUREMENT    0x21AC
+#define SCD4X_CMD_GET_DATA_READY_STATUS                   0xE4B8
+#define SCD4X_CMD_PERSIST_SETTINGS                        0x3615
+#define SCD4X_CMD_GET_SERIAL_NUMBER                       0x3682
+#define SCD4X_CMD_PERFORM_SELF_TEST                       0x3639
+#define SCD4X_CMD_PERFORM_FACTORY_RESET                   0x3632
+#define SCD4X_CMD_REINIT                                  0x3646
+#define SCD4X_CMD_MEASURE_SINGLE_SHOT                     0x219D
+#define SCD4X_CMD_MEASURE_SINGLE_SHOT_RHT_ONLY            0x2196
+#define SCD4X_CMD_POWER_DOWN                              0x36E0
+#define SCD4X_CMD_WAKE_UP                                 0x36F6
+
 typedef struct
 {
     uint16_t co2_ppm;
@@ -32,8 +53,7 @@ void SCD4X_init(I2C_HandleTypeDef *hi2c);
 HAL_StatusTypeDef  SCD4X_start_periodic_measurement();  //0x21b1 send command
 HAL_StatusTypeDef SCD4X_read_measurement(SCD4X_Measurement_t *out);		  //0xec05 read, 1ms execution // poll every 5 seconds
 HAL_StatusTypeDef  SCD4X_stop_periodic_measurement();   //0x3f86 send command, 500ms execution
-uint8_t SCD4X_generate_crc(const uint8_t* data, uint16_t count);
-void SCD4X_calculate_checksum();
+
 
 
 #endif /* INC_SCD4X_H_ */
